f9895ac5b4f12a64b73544aeefad32b4fbfb3c6f
Rimmel Botond <rimmelbotond@gmail.com>
2025-10-04 01:08:28 +0200

    modification in poll_transmit

--- quinn-proto/src/connection/mod.rs
+++ quinn-proto/src/connection/mod.rs
@@ -557,6 +557,14 @@ impl Connection {
 
                 admit_streams = admitted_streams;
 
+                if space_id == SpaceId::Data && !self.streams.can_send_stream_data() {
+                // Ha nincs ténylegesen küldhető stream frame (pl. minden szűrve lett),
+                // ne építsünk üres packetet ami 'other=true' státuszt sugall.
+                // Lépjünk tovább a következő space-re.
+                space_idx += 1;
+                continue;
+                }
+
                 let have_admitted_streams = !admit_streams.is_empty();
 
                 let space = &self.spaces[SpaceId::Data];
