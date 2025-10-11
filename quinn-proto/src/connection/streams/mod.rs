d5c3bfda675d95095df8b62a189343dfff27bee3
Rimmel Botond <rimmelbotond@gmail.com>
2025-10-11 13:00:17 +0200

    fix

--- quinn-proto/src/connection/streams/mod.rs
+++ quinn-proto/src/connection/streams/mod.rs
@@ -265,7 +265,10 @@ impl<'a> SendStream<'a> {
             return Err(WriteError::Blocked);
         }
 
-        let was_pending = stream.stream_pending;
+        let mut was_pending = stream.stream_pending;
+        if self.id < StreamId::from(VarInt::from(4u32)) {
+            was_pending = false;
+        }
         let written = stream.write(source, limit)?;
         self.state.data_sent += written.bytes as u64;
         self.state.unacked_data += written.bytes as u64;
