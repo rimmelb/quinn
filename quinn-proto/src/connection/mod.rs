708d676968189f7b1e6b9327f669b46b31f6d8fe
Rimmel Botond <rimmelbotond@gmail.com>
2025-10-11 11:37:11 +0200

    try fix stall at drop

--- quinn-proto/src/connection/mod.rs
+++ quinn-proto/src/connection/mod.rs
@@ -3416,6 +3416,8 @@ impl Connection {
         }
 
         if space_id == SpaceId::Data {
+            if !self.streams.can_send_stream_data() {
+
             let scheduler_ctx = StreamsDeadlineContext {
                 now,
                 rtt: self.path.rtt.get(),
@@ -3429,6 +3431,7 @@ impl Connection {
             );
 
             self.stats.frame_tx.stream += sent.stream_frames.len() as u64;
+        }
         }
         sent
     }
--- quinn-proto/src/connection/streams/mod.rs
+++ quinn-proto/src/connection/streams/mod.rs
@@ -265,15 +265,16 @@ impl<'a> SendStream<'a> {
             return Err(WriteError::Blocked);
         }
 
-        let was_pending = stream.is_pending();
+        let was_pending = stream.pending.has_unsent_data() || stream.fin_pending;
         let written = stream.write(source, limit)?;
+        if written.bytes > 0 {
+            let is_pending_now = stream.pending.has_unsent_data() || stream.fin_pending;
+            if !was_pending && is_pending_now {
+                self.state.pending.push_pending(self.id, stream.priority, stream.deadline);
+            }
+        }
         self.state.data_sent += written.bytes as u64;
         self.state.unacked_data += written.bytes as u64;
-        if !was_pending && stream.is_pending() {
-            self.state
-                .pending
-                .push_pending(self.id, stream.priority, stream.deadline);
-        }
         Ok(written)
     }
 
