d8390743521f8132352e0b4f0a1b3bc519a8561a
Rimmel Botond <rimmelbotond@gmail.com>
2025-10-16 18:37:53 +0200

    fix overflow error

--- quinn-proto/src/connection/streams/state.rs
+++ quinn-proto/src/connection/streams/state.rs
@@ -765,7 +765,7 @@ impl StreamsState {
 
             if should_drop_object {
                 let object_size = stream.object_sizes.as_mut().and_then(|hints| hints.pop_last_object().map(|obj| obj.total_len));
-                if (stream.pending.unacked() - object_size.unwrap_or(0)) < 140 {
+                if stream.pending.unacked().saturating_sub(object_size.unwrap_or(0)) < 140 {
                     should_drop_object = true;
                 }
                 else {
